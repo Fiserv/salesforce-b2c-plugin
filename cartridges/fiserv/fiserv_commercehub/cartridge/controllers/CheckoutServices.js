996ec039525bf81743261391af98dd7d647f6f17
Ian Harvey <ian.harvey@fiserv.com>
2025-11-03 15:32:51 -0500

    MCH Fixed backend error for CheckoutServices controller

--- cartridges/fiserv/fiserv_commercehub/cartridge/controllers/CheckoutServices.js
+++ cartridges/fiserv/fiserv_commercehub/cartridge/controllers/CheckoutServices.js
@@ -13,11 +13,13 @@ server.extend(module.superModule);
 
 server.append('SubmitPayment', function (req, res, next) {
     this.on('route:BeforeComplete', function (req, res) {
+        if(res.viewData.error)
+            return;
 
         // This is here to make sure that the listed payments being sent back to the frontend don't list the gift cards first
         // It is intended to be temporary code that is replaced with a better UI visualization for gift cards being applied to an order
         // If this is here a few months/years down the line from (Thursday, May 13th, 2025), then congratulations on graduating to legacy code
-        if(commercehubConfig.getCommerceHubGiftEnabled() && !res.viewData.error)
+        if(commercehubConfig.getCommerceHubGiftEnabled())
         {
             let selectedPaymentInstruments = res.viewData.order.billing.payment.selectedPaymentInstruments;
             if(selectedPaymentInstruments.length > 1)
@@ -31,7 +33,7 @@ server.append('SubmitPayment', function (req, res, next) {
         }
 
         let paymentMethod = res.viewData.paymentMethod.value;
-        if(!res.viewData.error && paymentMethod === constants.COMMERCEHUB_APPLEPAY_PAYMENT_METHOD && fiservHelper.isApplePayFiserv())
+        if(paymentMethod === constants.COMMERCEHUB_APPLEPAY_PAYMENT_METHOD && fiservHelper.isApplePayFiserv())
         {
             // Fake an error to prevent a page load...
             res.viewData.error = true;
@@ -48,7 +50,7 @@ server.append('SubmitPayment', function (req, res, next) {
         {
             return;
         }
-        if(req.currentCustomer.profile !== undefined && !res.viewData.error)
+        if(req.currentCustomer.profile !== undefined)
         {
             let profile = CustomerMgr.getCustomerByCustomerNumber(req.currentCustomer.profile.customerNo).getProfile();
             let paymentInstruments = null;
