55c40b98be22933b7b0320d8725200b0d6e7aac1
Ian Harvey <ian.harvey@fiserv.com>
2026-02-27 12:22:06 -0500

    MCH Added config fields for ACH

--- cartridges/fiserv/fiserv_commercehub/cartridge/fiservConstants/constants.js
+++ cartridges/fiserv/fiserv_commercehub/cartridge/fiservConstants/constants.js
@@ -17,6 +17,7 @@ module.exports = {
     TOKEN_SOURCE_TYPE : "PaymentToken",
     PROCESSOR_ID_LIST : {
         COMMERCEHUB_PROCESSOR : "FISERV_COMMERCEHUB",
+        COMMERCEHUB_ACH_PROCESSOR : "FISERV_COMMERCEHUB_ACH",
         COMMERCEHUB_GIFT_PROCESSOR : "FISERV_COMMERCEHUB_GIFT",
         COMMERCEHUB_PAYPAL_PROCESSOR : "FISERV_COMMERCEHUB_PAYPAL",
         COMMERCEHUB_VENMO_PROCESSOR : "FISERV_COMMERCEHUB_VENMO",
@@ -25,6 +26,7 @@ module.exports = {
         COMMERCEHUB_SAMSUNGPAY_PROCESSOR : "FISERV_COMMERCEHUB_SAMSUNGPAY",
     },
     PAYMENT_METHOD_LIST : {
+        COMMERCEHUB_ACH_PAYMENT_METHOD : "ACH",
         COMMERCEHUB_GIFT_PAYMENT_METHOD : "GIFT_CARD",
         COMMERCEHUB_APPLEPAY_PAYMENT_METHOD : "APPLEPAY",
         COMMERCEHUB_SAMSUNGPAY_PAYMENT_METHOD : "SAMSUNGPAY",
@@ -54,7 +56,7 @@ module.exports = {
         ERROR_MESSAGE: ['error', [0], 'message']
     },
     ICON_LIST : ['card', 'gear', 'gift', 'money', 'sign', 'token'],
-    FORM_ID_LIST : [ 'Payment', 'Tokenization', 'Gift' ],
+    FORM_ID_LIST : [ 'Payment', 'Tokenization', 'ACH', 'Gift' ],
     DEPENDENCY_LIST : {
         'CommerceHubCreditEnable': [
             'CommerceHubCreditPaymentType',
@@ -71,6 +73,9 @@ module.exports = {
         ],
         'CommerceHubEarlyTokenization': [
             'CommerceHubBasketTokenization'
+        ],
+        'CommerceHubACHEnable': [
+            'CommerceHubACHPaymentType'
         ],
         'CommerceHubGiftEnable': [
             'CommerceHubGiftPaymentMethodTitle',
@@ -102,7 +107,10 @@ module.exports = {
     },
     FORM_DEPENDENCY_LIST : {
         'CardNumberMask': ['CardNumberMaskCharacter', 'CardNumberMaskMode', 'CardNumberMaskLength'],
-        'SecurityCodeMask': ['SecurityCodeMaskCharacter', 'SecurityCodeMaskMode']
+        'SecurityCodeMask': ['SecurityCodeMaskCharacter', 'SecurityCodeMaskMode'],
+        'AccountNumberMask': ['AccountNumberMaskingCharacter', 'AccountNumberMaskingMode', 'AccountNumberShrunkLength'],
+        'RoutingNumberMask': ['RoutingNumberMaskingCharacter', 'RoutingNumberMaskingMode', 'RoutingNumberShrunkLength'],
+        'IdValueMask': ['IdValueMaskingCharacter', 'IdValueMaskingMode', 'IdValueShrunkLength']
     },
     CONFIG_VALIDATIONS : {
         MANDATORY: [ // A list of absolutely mandatory fields (Excluding select dropdowns...)
@@ -130,7 +138,8 @@ module.exports = {
             'CommerceHubPaymentFormExpirationMonthOptionLabels',
             'CommerceHubTokenizationFormExpirationMonthOptionLabels',
             'CommerceHubPaymentFormCSS',
-            'CommerceHubTokenizationFormCSS'
+            'CommerceHubTokenizationFormCSS',
+            'CommerceHubACHFormCSS'
         ]
     },
     CONFIG_DESCRIPTIONS : {
--- cartridges/fiserv/fiserv_commercehub/cartridge/scripts/utils/commercehubConfig.js
+++ cartridges/fiserv/fiserv_commercehub/cartridge/scripts/utils/commercehubConfig.js
@@ -112,6 +112,19 @@ const commerceHubExport =
         return getSitePreference('CommerceHub3DSEnable');
     },
 
+    // This is where the ACH settings start
+
+    getCommerceHubACHEnabled()
+    {
+        return getSitePreference('CommerceHubACHEnable');
+    },
+
+    getCommerceHubACHPaymentType()
+    {
+        return getSitePreference('CommerceHubACHPaymentType').value;
+    },
+
+
     // This is where the Gift Card settings start
 
     getCommerceHubGiftEnabled()
@@ -217,7 +230,7 @@ const commerceHubExport =
             return;
 
         let config = {};
-        config['fields'] = this.buildFormFieldsConfig(formId);
+        config['fields'] = formId !== "ACH" ? this.buildFormFieldsConfig(formId) : this.buildACHFormFieldsConfig();
         config['css'] = JSON.parse(getSitePreference('CommerceHub' + formId + 'FormCSS') || '{}');
         config['font'] = this.buildFormFontConfig(formId);
         config['contextualCssClassNames'] = {
@@ -282,6 +295,71 @@ const commerceHubExport =
             }
         }
 
+        return fieldsConfig;
+    },
+
+    buildACHFormFieldsConfig()
+    {
+        let fieldsConfig = {};
+        let elementIdPrefix = 'fiserv_commercehub-ach';
+
+        fieldsConfig['accountNumber'] = {
+            'parentElementId': elementIdPrefix + '-account-number',
+            'placeholder': getSitePreference('CommerceHubACHFormAccountNumberPlaceholder'),
+            'dynamicPlaceholderCharacter': getSitePreference('CommerceHubACHFormAccountNumberPlaceholderCharacter')?.value,
+            'masking': {
+                'character': getSitePreference('CommerceHubACHFormAccountNumberMaskingCharacter')?.value,
+                'mode': (getSitePreference('CommerceHubACHFormAccountNumberMask') 
+                        ? getSitePreference('CommerceHubACHFormAccountNumberMaskingMode')?.value : NO_MASKING),
+                'shrunkLength': getSitePreference('CommerceHubACHFormAccountNumberShrunkLength')
+            }
+        };
+    
+        fieldsConfig['routingNumber'] = {
+            'parentElementId': elementIdPrefix + '-routing-number',
+            'placeholder': getSitePreference('CommerceHubACHFormRoutingNumberPlaceholder'),
+            'dynamicPlaceholderCharacter': getSitePreference('CommerceHubACHFormRoutingNumberPlaceholderCharacter')?.value,
+            'masking': {
+                'character': getSitePreference('CommerceHubACHFormRoutingNumberMaskingCharacter')?.value,
+                'mode': (getSitePreference('CommerceHubACHFormRoutingNumberMask') 
+                        ? getSitePreference('CommerceHubACHFormRoutingNumberMaskingMode')?.value : NO_MASKING),
+                'shrunkLength': getSitePreference('CommerceHubACHFormRoutingNumberShrunkLength')
+            }
+        };
+    
+        fieldsConfig['idValue'] = {
+            'parentElementId': elementIdPrefix + '-id-value',
+            'placeholder': getSitePreference('CommerceHubACHFormIdValuePlaceholder'),
+            'dynamicPlaceholderCharacter': getSitePreference('CommerceHubACHFormIdValuePlaceholderCharacter')?.value,
+            'masking': {
+                'character': getSitePreference('CommerceHubACHFormIdValueMaskingCharacter')?.value,
+                'mode': (getSitePreference('CommerceHubACHFormIdValueMask') 
+                        ? getSitePreference('CommerceHubACHFormIdValueMaskingMode')?.value : NO_MASKING),
+                'shrunkLength': getSitePreference('CommerceHubACHFormIdValueShrunkLength')
+            }
+        };
+    
+        fieldsConfig['businessName'] = {
+            'parentElementId': elementIdPrefix + '-business-name',
+            'placeholder': getSitePreference('CommerceHubACHFormBusinessNamePlaceholder')
+        };
+    
+        fieldsConfig['idType'] = {
+            'parentElementId': elementIdPrefix + '-id-type'
+        };
+    
+        fieldsConfig['driverLicenseState'] = {
+            'parentElementId': elementIdPrefix + '-driver-license-state'
+        };
+    
+        fieldsConfig['accountType'] = {
+            'parentElementId': elementIdPrefix + '-account-type'
+        };
+    
+        fieldsConfig['checkType'] = {
+            'parentElementId': elementIdPrefix + '-check-type'
+        };
+    
         return fieldsConfig;
     },
 
