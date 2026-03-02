d7abaec381e4282517a8ea8a9f5a75525ee16594
Ian Harvey <ian.harvey@fiserv.com>
2026-03-02 16:31:06 -0500

    MCH Implemented most of the ACH frontend

--- cartridges/fiserv/fiserv_commercehub/cartridge/static/default/js/fiserv_credit/commercehub-credit-form.js
+++ cartridges/fiserv/fiserv_commercehub/cartridge/static/default/js/fiserv_credit/commercehub-credit-form.js
@@ -406,6 +406,7 @@ class CommercehubCheckoutForm
     watchSubmitResponse = function()
     {
         $(document).on("ajaxError", $.proxy(this.onSubmitResponse, this));
+        $(document).on("ajaxSuccess", $.proxy(this.onSubmitResponse, this));
     }
 
     onSubmitResponse = function(ev, xhr)
