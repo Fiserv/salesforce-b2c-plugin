14c7959a3258a741cb90466a9a2ca5df43011011
Ian Harvey <ian.harvey@fiserv.com>
2026-03-03 15:36:11 -0500

    MCH Added in legal text generation for ACH

--- cartridges/fiserv/fiserv_commercehub/cartridge/scripts/utils/commercehubConfig.js
+++ cartridges/fiserv/fiserv_commercehub/cartridge/scripts/utils/commercehubConfig.js
@@ -335,7 +335,8 @@ const commerceHubExport =
             'masking': {
                 'character': getSitePreference('CommerceHubACHFormAccountNumberMaskingCharacter').value,
                 'mode': (getSitePreference('CommerceHubACHFormAccountNumberMask') 
-                        ? getSitePreference('CommerceHubACHFormAccountNumberMaskingMode').value : NO_MASKING)
+                        ? getSitePreference('CommerceHubACHFormAccountNumberMaskingMode').value : NO_MASKING),
+                'shrunkLength': getSitePreference('CommerceHubACHFormAccountNumberMaskLength')
             }
         };
     
@@ -346,7 +347,8 @@ const commerceHubExport =
             'masking': {
                 'character': getSitePreference('CommerceHubACHFormRoutingNumberMaskingCharacter').value,
                 'mode': (getSitePreference('CommerceHubACHFormRoutingNumberMask') 
-                        ? getSitePreference('CommerceHubACHFormRoutingNumberMaskingMode').value : NO_MASKING)
+                        ? getSitePreference('CommerceHubACHFormRoutingNumberMaskingMode').value : NO_MASKING),
+                'shrunkLength': getSitePreference('CommerceHubACHFormRoutingNumberMaskLength')
             }
         };
     
