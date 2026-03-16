8a69c8d7a479726ae54450b37588bfe42deb0d63
Ian Harvey <ian.harvey@fiserv.com>
2026-03-16 15:24:21 -0400

    MCH Re-arranged apple pay frontend structure to allow for modularization between checkout and express flow

---
+++ cartridges/fiserv/fiserv_commercehub/cartridge/static/default/js/fiserv_applepay/commercehub-applepay-button-base.js
@@ -0,0 +1,81 @@
+'use strict';
+
+class CommercehubApplePay
+{
+
+    constructor(initializationData)
+    {
+        if (typeof(initializationData) === "undefined")
+        {
+            throw new Error("Initialization Data not found. Unable to initialize Apple Pay button.");
+        }
+
+        this.configDataApplePay = initializationData.config.configData;
+        this.configDataApplePay.buttonConfig.button['locale'] = initializationData.locale.replace('_', '-');
+        this.credentialsUrl = initializationData.credentialsUrl;
+
+        this.createAdapter();
+        this.CommercehubApplePayEventHandler = new CommercehubApplePayEventHandler(initializationData, this.sdkButton, this);
+    }
+
+    initialize = async function()
+    {
+        try {
+            $.spinner().start();
+            $('#fiserv-applepay-fatal-notice').hide();
+            await this.sdkButton.initSdk(this.credentialsUrl, this.setSessionIdInput, "ApplePay");
+            this.CommercehubApplePayEventHandler.initialize();
+        } catch (_err) {
+            this.sdkLoadFailure(_err);
+        }
+    }
+
+    createAdapter = function()
+    {
+        let loadSuccessCallback = () => { console.log("CommerceHub Apple Pay SDK has loaded."); };
+        let loadFailCallback = (error) => { this.sdkLoadFailure(error); };
+        let sdkReadyCallback = () => { this.sdkInitialized() };
+
+        this.sdkButton = new FiservSDKButton(
+            loadSuccessCallback,
+            loadFailCallback,
+            sdkReadyCallback
+        );
+    }
+
+    createCallbacksObject = function()
+    {
+        return {
+            onApprove: (response) => { this.CommercehubApplePayEventHandler.handleApproval(response); },
+            onCancel: (response) => { this.CommercehubApplePayEventHandler.handleCancel(response); },
+            onError: (response) => { this.CommercehubApplePayEventHandler.handleError(response); }
+        };
+    }
+
+    sdkInitialized = async function()
+    {
+        try
+        {
+            await window.fiserv.components.applePay({ data: this.configDataApplePay.buttonConfig, hooks: this.createCallbacksObject() });
+        }
+        catch(e)
+        {
+            console.log(e);
+            $('#fiserv-applepay-fatal-notice').show();
+        }
+        $.spinner().stop();
+    }
+
+    sdkLoadFailure = function (err) 
+    {
+        console.log(err);
+        $('#fiserv-applepay-fatal-notice').show();
+        $.spinner().stop();
+        throw new Error("Unable to load CommerceHub SDK.")
+    }
+
+    setSessionIdInput = function(sessionId)
+    {
+        $('input#commercehubSessionIdInputApplePay').val(sessionId);
+    }
+}
