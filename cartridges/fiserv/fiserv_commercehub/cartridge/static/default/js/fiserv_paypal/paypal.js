df93a9be1f8f754b5d809710b9a75fcb41f4df70
santhoshkumar.devasani <santhoshkumar.devasani@fiserv.com>
2026-04-22 11:43:27 -0400

    COM MCH-30221 Paypal button is not loading automatically if only one payment method selected, User has to click on the paypal tab

--- cartridges/fiserv/fiserv_commercehub/cartridge/static/default/js/fiserv_paypal/paypal.js
+++ cartridges/fiserv/fiserv_commercehub/cartridge/static/default/js/fiserv_paypal/paypal.js
@@ -1,6 +1,8 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const checkoutStage = $('#fiserv-commercehub-paypal-form-init-container').attr('data-initial-checkout-stage');
+
     let extractInitializationData = function()
     {
         let data = {
@@ -55,6 +57,14 @@ document.addEventListener("DOMContentLoaded", () => {
         if($(".payment-information").data("payment-method-id") === "PAYPAL")
             initPayPal();
     })
+
+    if ($(".payment-information").data("payment-method-id") === "PAYPAL") {
+        switch (checkoutStage) {
+            case 'payment':
+                initPayPal();
+                break;
+        }
+    }
 
     let grandTotalUpdated = function()
     {
